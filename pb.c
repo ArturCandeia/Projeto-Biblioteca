d7dfb44deab3e49eb8cebd59baf11adfc7b0eba9
Pedro Emanuel <pedroemanuel_07@hotmail.com>
2022-08-29 14:38:15 -0300

    projetoV1

---
+++ pb.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+// Controle de telas 
+void tela_inicial(void);
+void tela_sobre(void);
+void tela_equipe(void);
+
+// Programa 
+int main(void){
+    void tela_inicial(void);
+    void tela_sobre(void);
+    void tela_equipe(void);
+}
+   
+// Funções 
+void tela_inicial(void){
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///                 Bacharelado Sistemas de Informação                      ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///               Projeto Sistema de Controle de Biblioteca                 ///\n");
+    printf("///             Developed by @Pedro-E-Rodrigues and @ -- 2022               ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            > > > >  Sistema de Controle de Biblioteca < < < <           ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Módulo Usuários                                           ///\n");
+    printf("///            2. Módulo Livros                                             ///\n");
+    printf("///            3. Módulo Empréstimos                                        ///\n");
+    printf("///            4. Módulo Devoluções                                         ///\n");
+    printf("///            5. Módulo Funcionários                                       ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+void tela_sobre(void){
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            > > > > Sistema de Controle de Biblioteca < < < <            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Este Programa tem como base servir para o controle e gestão para       ///\n");
+    printf("///  uma biblioteca. Nessa versão do programa ele contem a ídeia básica     ///\n");
+    printf("///  para o projeto que iremos desenvolver com o decorrer da disciplina     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+void tela_equipe(void){
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            > > > > Sistema de Controle de Biblioteca < < < <            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Projeto desenvolvido por:                                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///      Alunos: Pedro Emanuel de Brito Rodrigues                           ///\n");
+    printf("///      E-mail: pedro.rodrigues.129@ufrn.edu.br                            ///\n");
+    printf("///      Redes sociais: @pedro__emanuel__                                   ///\n");
+    printf("///      Git: https://github.com/Pedro-E-Rodrigues/Projeto-Biblioteca.git   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
