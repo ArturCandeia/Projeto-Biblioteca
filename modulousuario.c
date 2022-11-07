cc83e1295d29ba504a4c02695eeb8586e5538dd9
Pedro Emanuel <pedroemanuel_07@hotmail.com>
2022-11-07 17:59:00 -0300

    arquivos porem com defeito

--- modulousuario.c
+++ modulousuario.c
@@ -3,14 +3,7 @@
 #include <unistd.h>
 #include "modulousuario.h"
 
-struct usuarios {
-  char esc;
-  char cpf[12]; 
-  char nome[51];
-  char email[51];
-  char nasc[11];
-  char celular[12];
-};
+typedef struct usuario Usuario;
 
 void menu_nav_usuario(void)
 {
@@ -74,75 +67,195 @@ char tela_usuarios(void){
     return esc;
     
 }
-char tela_cadastrar_u(void){
-  struct usuarios clientes;
-    system("clear||cls");
+
+void salArq_Usuario(Usuario* usu){
+    FILE *fp;
+fp = fopen("arqUsuario.dat","ab");
+
+if (fp == NULL){
+
+      fp = fopen("arqUsuario.dat","wb");
+      printf("Arquivo inexistente!\n");
+      printf("Criando novo arquivo!");
+
+      if (fp == NULL) {
+        printf("Erro com arquivo!");
+
+      }
+
+      else {
+        fwrite(usu, sizeof(Usuario), 1, fp);
+
+      }
+
+    }
+
+    else {
+        fwrite(usu, sizeof(Usuario), 1, fp);
+
+    }
+
+    fclose(fp);
+
+}
+
+ void lerArq_Usuario(void) {
+    FILE *fp;
+    Usuario *usu;
+    usu = (Usuario*) malloc(sizeof(Usuario));
+
+    fp = fopen("arqUsuario.dat","rb");
+
+    if (fp == NULL) {
+
+        fp = fopen("arqUsuario.dat","wb");
+        printf("Arquivo inexistente!\n");
+        printf("Criando novo arquivo!");
+
+        if (fp == NULL) {
+            printf("Erro com arquivo!");
+
+        }
+
+    }
+
+    else {
+
+        while (fread(usu, sizeof(Usuario), 1, fp)) {
+
+            if (usu->status != 0) {
+
+                fread(usu, sizeof(Usuario), 1, fp);
+                
+                exibeUsuario(usu);
+
+
+            }
+        }
+    }
+
+    free(usu);
+    fclose(fp);
+
+}
+
+Usuario* achar_Usuario(char *cpf) {
+    FILE* fp;
+    Usuario* usu;
+
+    usu = (Usuario*) malloc(sizeof(Usuario));
+    fp = fopen("arqUsuario.dat", "rb");
+
+    if (fp == NULL) {
+        printf("Ocorreu um erro na abertura do arquivo!\n");
+
+    }
+
+
+    else {
+
+        while(!feof(fp)) {
+            fread(usu, sizeof(Usuario), 1, fp);
+
+            if (strcmp(usu->cpf, cpf) == 0) {
+
+                if (usu->status != 0) {
+                    fclose(fp);
+                    return usu;
+                }
+
+                else {
+                    fclose(fp);
+                    return NULL;
+                }
+
+            } 
+
+        }
+
+    }
+
+    fclose(fp);
+    return NULL;
+}
+ 
+void exibe_Usuario(Usuario *usu) {
+
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            > > > > Sistema de Controle de Biblioteca < < < <            ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///           Developed by @pedro__emanuel__ and @ArturCandeia --2022       ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
+    printf("Nome do Usuário: ");
+    printf("%s" ,usu->nome);
+    printf("\n");
+    printf("CPF do Usuário: ");
+    printf("%s" ,usu->cpf);
+    printf("\n");
+    printf("Email do Usuário: ");
+    printf("%s" ,usu->email);
+    printf("\n");
+    printf("Número do Usuário: ");
+    printf("%s" ,usu->celular);
+    printf("\n");
+    printf("Data de nascimento do Usuário");
+    printf("%s" ,usu->nasc);
+    printf("\n");
+}
+
+
+
+
+    Usuario* tela_cadastrar_u(void){
+    Usuario* usu;
+    usu = (Usuario*) malloc(sizeof(Usuario));
+
+    system("clear||cls");
+    printf("\n");                                                                       
     printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
     printf("///            = = = = = = = = Cadastro de úsuario = = = = = = = =          ///\n");
     printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
     printf("///                                                                         ///\n");
     printf("///           CPF (apenas números): ");
-    scanf(" %11[^\n]", clientes.cpf);
+    scanf(" %11[^\n]", usu->cpf);
     printf("///           Nome completo: ");
-    scanf(" %50[^\n]", clientes.nome);
+    scanf(" %50[^\n]", usu->nome);
     printf("///           E-mail: ");
-    scanf(" %50[^\n]", clientes.email);
+    scanf(" %50[^\n]", usu->email);
     printf("///           Data de Nascimento (dd/mm/aaaa):  ");
-    scanf(" %10[^\n]", clientes.nasc);
+    scanf(" %10[^\n]", usu->nasc);
     printf("///           Celular  (apenas números): ");
-    scanf(" %11[^\n]", clientes.celular);
-    scanf("%c", &clientes.esc);  
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-    return clientes.esc;
+    scanf(" %11[^\n]", usu->celular);
+    usu->status = 'm';
+    return usu;
 }
 
-char tela_pesquisar_u(void){
-    char esc;
-    char cpf[12]; 
-
+void tela_pesquisar_u(void){
+    FILE* fp;
+    Usuario* usu;
+    char cpf[12];
+    
     system("clear||cls");
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            > > > > Sistema de Controle de Biblioteca < < < <            ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///           Developed by @pedro__emanuel__ and @ArturCandeia --2022       ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
     printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
     printf("///            = = = = = = = = Pesquisar Usuário = = = = = = = =            ///\n");
     printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
     printf("///                                                                         ///\n");
     printf("///           Informe o CPF (apenas números): ");
-    scanf("%[0-9]", cpf);
-    getchar();
-    scanf("%c", &esc);
-    getchar();   
-    printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-    getchar();
-    return esc;
+    scanf("%d", &cpf);
+    usu = (Usuario*) malloc(sizeof(Usuario));
+    fp = fopen("arqUsuario.dat", "rb");
+    if (fp == NULL) {
+    printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+    printf("Não é possível continuar este programa...\n");
+    exit(1);
+  }
+  while(!feof(fp)) {
+    fread(usu, sizeof(Usuario), 1, fp);
+    if ((usu->cpf == cpf) && (usu->status != 'x')) {
+      fclose(fp);
+      return usu;
+    }
+  }
+  fclose(fp);
+  return NULL;
 }
 
 char tela_editar_u(void){
@@ -151,16 +264,6 @@ char tela_editar_u(void){
 
     system("clear||cls");
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            > > > > Sistema de Controle de Biblioteca < < < <            ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///           Developed by @pedro__emanuel__ and @ArturCandeia --2022       ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
     printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
     printf("///            = = = = = = = = Editar Usuário = = = = = = = =               ///\n");
@@ -181,36 +284,36 @@ char tela_editar_u(void){
 }
 
 
-char tela_excluir_u(void){
-    char esc;
+void tela_excluir_u(Usuario* usuLido) {
+    FILE* fp;
+    Usuario* usu;
     char cpf[12];
-    system("clear||cls");
-    printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            > > > > Sistema de Controle de Biblioteca < < < <            ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
-    printf("///            ===================================================          ///\n");
-    printf("///           Developed by @pedro__emanuel__ and @ArturCandeia --2022       ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///            = = = = = = = = excluir Usuário = = = = = = = =              ///\n");
-    printf("///            = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///           Informe o CPF (apenas números): ");
-    scanf("%[0-9]", cpf);
-    getchar();
-    scanf("%c", &esc);
-    getchar();   
-    printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-    getchar();
-    return esc;
+    int achou = 0;
+    if (usuLido == NULL) {
+    printf(" O Usuário informado não existe!\n");
+    }
+    else {
+    usu = (Usuario*) malloc(sizeof(Usuario));
+    fp = fopen("arqUsuario.dat", "r+b");
+    if (fp == NULL) {
+      printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+      printf("Não é possível continuar este programa...\n");
+      exit(1);
+    }
+    while(!feof(fp)) {
+      fread(usu, sizeof(Usuario), 1, fp);
+      if ((usu->cpf == usuLido->cpf) && (usu->status != 'x')) {
+        achou = 1;
+        usu->status = 'x';
+        fseek(fp, -1*sizeof(Usuario), SEEK_CUR);
+        fwrite(usu, sizeof(Usuario), 1, fp);
+        printf("\nUsuário excluído com sucesso!!!\n");
+      }
+    }
+    if (!achou) {
+      printf("\nUsuário não encontrado!\n");
+    }
+    fclose(fp);
+    free(usu);
+  }
 }
--- modulousuario.h
+++ modulousuario.h
@@ -1,11 +1,24 @@
+typedef struct usuario Usuario;
+
+struct usuario {
+  char esc;
+  char cpf[12]; 
+  char nome[51];
+  char email[51];
+  char nasc[11];
+  char celular[12];
+  char status;
+};
+
+
 char tela_usuarios(void);
 
-char tela_cadastrar_u(void);
+Usuario* tela_cadastrar_u(void);
 
-char tela_pesquisar_u(void);
+void tela_pesquisar_u(void);
 
 char tela_editar_u(void);
 
-char tela_excluir_u(void);
+void tela_excluir_u(void);
 
 void menu_nav_usuario(void);
